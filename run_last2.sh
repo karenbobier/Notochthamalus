69331e266604f88f3b5d752fa857f5c9e89292c4
karenbobier <karenbobier@gmail.com>
2019-03-18 11:00:39 -0400

    change job name, out and err files. increase wall time

--- run_last2.sh
+++ run_last2.sh
@@ -1,13 +1,13 @@
 #PBS -S /bin/bash
 #PBS -q batch
-#PBS -N last_noto
+#PBS -N last2_noto
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=7:00:00:00
+#PBS -l walltime=10:00:00:00
 #PBS -l mem=100gb
 #PBS -M keb27269@uga.edu
 #PBS -m ae
-#PBS -o $HOME/last.out.$PBS_JOBID
-#PBS -e $HOME/last.err.$PBS_JOBID
+#PBS -o $HOME/last2.out.$PBS_JOBID
+#PBS -e $HOME/last2.err.$PBS_JOBID
 
 basedir="/scratch/keb27269/noto/"
 #mkdir $basedir
