b050ea5676c08005881f1d21c93ee99c372d32c4
karenbobier <karenbobier@gmail.com>
2019-03-18 13:13:00 -0400

    change file paths and base dir

--- run_last2.sh
+++ run_last2.sh
@@ -9,7 +9,7 @@
 #PBS -o $HOME/last2.out.$PBS_JOBID
 #PBS -e $HOME/last2.err.$PBS_JOBID
 
-basedir="/scratch/keb27269/noto/"
+basedir="/scratch/keb27269/noto/last/"
 #mkdir $basedir
 cd $basedir
 
@@ -17,44 +17,44 @@ module load LAST/956-foss-2016b
 #module load LAST/959-foss-2018a
 module load SAMtools/1.9-foss-2016b
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI4.sam > last/ARI4.bam
-samtools sort last/ARI4.bam last/sortARI4
+samtools view -bt filtered_Trinity.fasta.fai ARI4.sam > ARI4.bam
+samtools sort ARI4.bam sortARI4
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI6.sam > last/ARI6.bam
-samtools sort last/ARI6.bam last/sortARI6
+samtools view -bt filtered_Trinity.fasta.fai ARI6.sam > ARI6.bam
+samtools sort ARI6.bam sortARI6
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI10.sam > last/ARI10.bam
-samtools sort last/ARI10.bam last/sortARI10
+samtools view -bt filtered_Trinity.fasta.fai ARI10.sam > ARI10.bam
+samtools sort ARI10.bam sortARI10
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI11.sam > last/ARI11.bam
-samtools sort last/ARI11.bam last/sortARI11
+samtools view -bt filtered_Trinity.fasta.fai ARI11.sam > ARI11.bam
+samtools sort ARI11.bam sortARI11
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI12.sam > last/ARI12.bam
-samtools sort last/ARI12.bam last/sortARI12
+samtools view -bt filtered_Trinity.fasta.fai ARI12.sam > ARI12.bam
+samtools sort ARI12.bam sortARI12
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG10.sam > last/ARG10.bam
-samtools sort last/ARG10.bam last/sortARG10
+samtools view -bt filtered_Trinity.fasta.fai ARG10.sam > ARG10.bam
+samtools sort ARG10.bam sortARG10
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG18.sam > last/ARG18.bam
-samtools sort last/ARG18.bam last/sortARG18
+samtools view -bt filtered_Trinity.fasta.fai ARG18.sam > ARG18.bam
+samtools sort ARG18.bam sortARG18
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG19.sam > last/ARG19.bam
-samtools sort last/ARG19.bam last/sortARG19
+samtools view -bt filtered_Trinity.fasta.fai ARG19.sam > ARG19.bam
+samtools sort ARG19.bam sortARG19
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG21.sam > last/ARG21.bam
-samtools sort last/ARG21.bam last/sortARG21
+samtools view -bt filtered_Trinity.fasta.fai ARG21.sam > ARG21.bam
+samtools sort ARG21.bam sortARG21
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG25.sam > last/ARG25.bam
-samtools sort last/ARG21.bam last/sortARG25
+samtools view -bt filtered_Trinity.fasta.fai ARG25.sam > ARG25.bam
+samtools sort ARG21.bam sortARG25
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG27.sam > last/ARG27.bam
-samtools sort last/ARG27.bam last/sortARG27
+samtools view -bt filtered_Trinity.fasta.fai ARG27.sam > ARG27.bam
+samtools sort ARG27.bam sortARG27
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARG29.sam >last/ARG29.bam
-samtools sort last/ARG29.bam last/sortARG29
+samtools view -bt filtered_Trinity.fasta.fai ARG29.sam >ARG29.bam
+samtools sort ARG29.bam sortARG29
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI102.sam > last/ARI102.bam
-samtools sort last/ARI102.bam last/sortARI102
+samtools view -bt filtered_Trinity.fasta.fai ARI102.sam > ARI102.bam
+samtools sort ARI102.bam sortARI102
 
-samtools view -bt filtered_Trinity.fasta.fai last/ARI103.sam > last/ARI103.bam
-samtools sort last/ARI103.bam last/sortARI103
+samtools view -bt filtered_Trinity.fasta.fai ARI103.sam > ARI103.bam
+samtools sort ARI103.bam sortARI103
