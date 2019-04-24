3a363bdf7a6ede62ae29dd67883934edad21791d
karenbobier <karenbobier@gmail.com>
2019-04-24 17:15:48 -0400

    remove -T and move -m... to same line as command

---
+++ 2_run_transdecoder.sh
@@ -0,0 +1,26 @@
+#PBS -S /bin/bash
+#PBS -q highmem_q
+#PBS -N 1_transdecoder_noto
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=30:00:00:00
+#PBS -l mem=200gb
+#PBS -M keb27269@uga.edu
+#PBS -m abe
+#PBS -o $HOME/1_transdecoder_noto.out.$PBS_JOBID
+#PBS -e $HOME/1_transdecoder_noto.err.$PBS_JOBID
+
+basedir="/scratch/keb27269/noto/"
+#mkdir $basedir
+cd $basedir
+
+module load TransDecoder/2.1.0-foss-2016b-Perl-5.24.1
+module load HMMER/3.1b2-foss-2016b
+module load BLAST+/2.7.1-foss-2016b-Python-2.7.14
+
+blastp -query transdecoder_dir/longest_orfs.pep  \
+    -db uniprot_sprot.fasta  -max_target_seqs 1 \
+    -outfmt 6 -evalue 1e-5 -num_threads 10 > blastp.outfmt6
+
+hmmscan --cpu 8 --domtblout pfam.domtblout /path/to/Pfam-A.hmm transdecoder_dir/longest_orfs.pep
+
+TransDecoder.Predict -t transcripts.fasta
