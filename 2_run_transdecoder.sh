42bd6cb326b5f3b077d93d786d3cde6e1386c0a6
karenbobier <karenbobier@gmail.com>
2019-04-24 19:45:17 -0400

    update paths

--- 2_run_transdecoder.sh
+++ 2_run_transdecoder.sh
@@ -9,7 +9,7 @@
 #PBS -o $HOME/2_transdecoder_noto.out.$PBS_JOBID
 #PBS -e $HOME/2_transdecoder_noto.err.$PBS_JOBID
 
-basedir="/scratch/keb27269/noto/gacrctest2/assemblies/"
+basedir="/scratch/keb27269/noto/"
 #mkdir $basedir
 cd $basedir
 
@@ -18,9 +18,9 @@ module load HMMER/3.1b2-foss-2016b
 module load BLAST+/2.7.1-foss-2016b-Python-2.7.14
 
 blastp -query /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder_dir/longest_orfs.pep  \
-    -db /blastdb/uniprot_sprot.fasta  -max_target_seqs 1 \
+    -db uniprot_sprot.fasta  -max_target_seqs 1 \
     -outfmt 6 -evalue 1e-5 -num_threads 20 > blastp.outfmt6
 
-hmmscan --cpu 20 --domtblout pfam_db/pfam.domtblout /scratch/keb27269/noto/pfam_db/Pfam-A.hmm /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder_dir/longest_orfs.pep
+hmmscan --cpu 20 --domtblout pfam.domtblout /scratch/keb27269/noto/Pfam-A.hmm /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder_dir/longest_orfs.pep
 
-TransDecoder.Predict -t /scratch/keb27269/noto/gacrctest2/assemblies/noto_1.5.ORP.fasta --retain_pfam_hits /scratch/keb27269/noto/pfam_db/pfam.domtblout --retain_blastp_hits blastp.outfmt6
+TransDecoder.Predict -t /scratch/keb27269/noto/gacrctest2/assemblies/noto_1.5.ORP.fasta --retain_pfam_hits pfam.domtblout --retain_blastp_hits blastp.outfmt6
