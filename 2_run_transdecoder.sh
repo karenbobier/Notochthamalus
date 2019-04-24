a8eea209cfaa789700adcf09035bb326856a30a2
karenbobier <karenbobier@gmail.com>
2019-04-24 19:34:06 -0400

    change path to query

--- 2_run_transdecoder.sh
+++ 2_run_transdecoder.sh
@@ -17,7 +17,7 @@ module load TransDecoder/2.1.0-foss-2016b-Perl-5.24.1
 module load HMMER/3.1b2-foss-2016b
 module load BLAST+/2.7.1-foss-2016b-Python-2.7.14
 
-blastp -query /longest_orfs.pep  \
+blastp -query /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder_dir/longest_orfs.pep  \
     -db blastdb/uniprot_sprot.fasta  -max_target_seqs 1 \
     -outfmt 6 -evalue 1e-5 -num_threads 20 > blastp.outfmt6
 
