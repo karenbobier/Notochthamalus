86a5c31c9ea97d5a0ba441e7d3460ae3278b6a65
karenbobier <karenbobier@gmail.com>
2019-11-01 16:01:02 -0400

    fixed path to .cds file

--- run_last3.sh
+++ run_last3.sh
@@ -21,30 +21,30 @@ module load BCFtools/1.9-foss-2016b
 
 mkdir /scratch/keb27269/noto/last/consensus/
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI4.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI4.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI4.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI4.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI6.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI6.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI6.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI6.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI10.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI10.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI10.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI10.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI11.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI11.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI11.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI11.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI12.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI12.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI12.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI12.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG10.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG10.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG10.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG10.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG18.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG18.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG18.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG18.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG19.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG19.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG19.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG19.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG21.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG21.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG21.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG21.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG25.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG25.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG25.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG25.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG27.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG27.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG27.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG27.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARG29.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG29.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARG29.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARG29.fq
 
-samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI102.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI102.fq
+samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI102.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI102.fq
 
-#samtools mpileup -uf /scratch/keb27269/notonoto_1.5.ORP.fasta.transdecoder.cds sortARI103.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI103.fq
+#samtools mpileup -uf /scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds sortARI103.bam | bcftools call -c | vcfutils.pl vcf2fq > consensus/ARI103.fq
