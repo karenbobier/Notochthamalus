49c000caeeeac56fd2b3d73935be8a664d84acd0
karenbobier <karenbobier@gmail.com>
2020-03-11 12:14:33 -0400

    scritp to run bqsr

--- gatk_pipeline/read_me.md
+++ gatk_pipeline/read_me.md
@@ -1,4 +1,6 @@
-scripts in this folder are based on a tutorial for using gatk to mark duplicates
+scripts in this folder are to analyse sequence data from fastq files to generating vcf files
+
+scripts 1-3 are based on a tutorial for using gatk to mark duplicates
 
 https://gatkforums.broadinstitute.org/gatk/discussion/2799#latest
 
@@ -8,10 +10,14 @@ script3.sh runs MarkDuplicates
 
 
 #################################################
-Next we need to run HaplotypeCaller
+Next we need to run HaplotypeCaller and make vcf files
 
-script4.sh will write and submit a script  to run HaplotypeCaller for each sample
+script4.sh will write and submit a script  to run HaplotypeCaller for each sample and produce a gvcf file
 
 script5.sh will combine the individual gvcf files into one and make a joint vcf files
 
 script6.sh will calculate the coverage in the bam files
+
+script7.sh will create a table of variants from a vcf
+
+script8.sh will run bqsr and output a recalibrated .bam file
---
+++ gatk_pipeline/script8.sh
@@ -0,0 +1,52 @@
+#PBS -S /bin/bash
+#PBS -q  highmem_q
+#PBS -N noto_gakt8
+#PBS -l nodes=1:ppn=6
+#PBS -l walltime=4:00:00:00
+#PBS -l mem=200gb
+#PBS -M keb27269@uga.edu
+#PBS -m abe
+#PBS -o $HOME/noto_gatk.out.$PBS_JOBID
+#PBS -e $HOME/noto_gatk.err.$PBS_JOBID
+
+#set base directory and move there
+basedir="/scratch/keb27269/noto/"
+#mkdir $basedir
+cd $basedir
+
+#load programs
+samtools_module="SAMtools/1.9-foss-2016b"
+module load Python/3.5.2-foss-2016b
+#location of picard module
+picard_module="picard/2.4.1-Java-1.8.0_144"
+#location of GATK module
+GATK_module="GATK/4.0.3.0-Java-1.8.0_144"
+#location of BWA module
+bwa_module="BWA/0.7.15-foss-2016b"
+
+module load ${picard_module}
+module load ${bwa_module}
+module load ${samtools_module}
+module load ${GATK_module}
+
+#set path to reference genome (noto transcritome cds file)
+ref_genome="/scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.cds.fasta"
+
+######################################
+#This script will run base quality score recalibraiton
+##########################################
+mkdir ${basedir}recalibrated
+
+for file in ${basedir}removed_duplicates/*_removedDuplicates.bam
+do
+
+FBASE=$(basename $file _removedDuplicates.bam)
+BASE=${FBASE%_removedDuplicates.bam}
+
+gatk ApplyBQSR \
+  -R ${ref_genome} \
+  -I ${basedir}removed_duplicates/${BASE}_removedDuplicates.bam \
+  -bsqr ${basedir}${BASE}_recal_data.table \
+  -O ${basedir}recalibrated/${BASE}_recalibrated.bam
+
+done
