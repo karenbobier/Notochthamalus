8e9904a441185c9f6b68101709635b6a236434a2
karenbobier <karenbobier@gmail.com>
2020-05-12 16:25:58 -0400

    edit phred calll command again

--- gatk_pipeline/script4_rna.sh
+++ gatk_pipeline/script4_rna.sh
@@ -75,7 +75,7 @@ echo "time gatk HaplotypeCaller \
 -ploidy 2 \
 --dont-use-soft-clipped-bases \
 -sample-name ${BASE} \
--‐stand_emit_conf 20.0 \
+--standard-min-confidence-threshold-for-calling 20.0 \
 -O ${basedir}gvcfs_rna/${BASE}_haplotypes.g.vcf" >> ${OUT}
 qsub ${OUT}
 
