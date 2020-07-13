48008637b05317c31ef7e0524ba1b49861188822
karenbobier <karenbobier@gmail.com>
2020-07-13 10:11:41 -0400

    run using the original ORP assembly as reference

--- snpGenie_scripts/run_snpGenie.sh
+++ snpGenie_scripts/run_snpGenie.sh
@@ -18,8 +18,13 @@ cd $basedir
 file="cds_seq_ids.txt"
 sequence_ids=$(cat ${file})
 snp_genie_path="/home/keb27269/projects/SNPGenie/snpgenie.pl"
-
+#use cds fasta as reference
+# for ID in $sequence_ids
+#   do
+#     $snp_genie_path --vcfformat=1 --snpreport="./variants/$ID.vcf" --fastafile="./reference_fasta/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs/$ID/"
+# done
+#use original ORP assembly as reference
 for ID in $sequence_ids
   do
-    $snp_genie_path --vcfformat=1 --snpreport="./variants/$ID.vcf" --fastafile="./reference_fasta/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs/$ID/"
+    $snp_genie_path --vcfformat=1 --snpreport="./variants_rna_edit/$ID.vcf" --fastafile="./reference_fastas_ORP/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs_ORP/$ID/"
 done
