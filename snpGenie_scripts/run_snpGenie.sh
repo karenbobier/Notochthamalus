e2e7e1d4f3c4526e98b7bea84de41e149ea2d994
karenbobier <karenbobier@gmail.com>
2020-07-17 08:43:05 -0400

    edit to run snpgenie for minus strand sequences

--- snpGenie_scripts/run_snpGenie.sh
+++ snpGenie_scripts/run_snpGenie.sh
@@ -15,16 +15,25 @@ basedir="/scratch/keb27269/noto/snpGenie_test"
 cd $basedir
 
 #file="test_seq_ids.txt"
-file="cds_seq_ids.txt"
-sequence_ids=$(cat ${file})
-snp_genie_path="/home/keb27269/projects/SNPGenie/snpgenie.pl"
-#use cds fasta as reference
+# file="cds_seq_ids.txt"
+# sequence_ids=$(cat ${file})
+# snp_genie_path="/home/keb27269/projects/SNPGenie/snpgenie.pl"
+# #use cds fasta as reference
+# # for ID in $sequence_ids
+# #   do
+# #     $snp_genie_path --vcfformat=1 --snpreport="./variants/$ID.vcf" --fastafile="./reference_fasta/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs/$ID/"
+# # done
+# #use original ORP assembly as reference
 # for ID in $sequence_ids
 #   do
-#     $snp_genie_path --vcfformat=1 --snpreport="./variants/$ID.vcf" --fastafile="./reference_fasta/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs/$ID/"
+#     $snp_genie_path --vcfformat=1 --snpreport="./variants_rna_edit/$ID.vcf" --fastafile="./reference_fastas_ORP/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs_ORP/$ID/"
 # done
-#use original ORP assembly as reference
+
+#run for reverse complement of minus strand seqs
+file="seq_ids_minus_strand2.txt"
+sequence_ids=$(cat ${file})
+snp_genie_path="/home/keb27269/projects/SNPGenie/snpgenie.pl"
 for ID in $sequence_ids
   do
-    $snp_genie_path --vcfformat=1 --snpreport="./variants_rna_edit/$ID.vcf" --fastafile="./reference_fastas_ORP/noto_1.5.ORP_$ID.fasta" --gtffile="./gff_files/$ID.gtf"  --outdir="./snp_genie_outputs_ORP/$ID/"
+    $snp_genie_path --vcfformat=1 --snpreport="./variants_rna_edit/$ID_revcom.vcf" --fastafile="./rev_com_fastas/noto_1.5.ORP_$ID_revcom.fasta" --gtffile="./gff_files/$ID_revcom.gtf"  --outdir="./snp_genie_outputs_ORP/rev_com/$ID/"
 done
