d7ede57b2b58b7e22aa4e826129f9542c2951ca3
karenbobier <karenbobier@gmail.com>
2020-08-03 14:50:37 -0400

    edit to use diploid number of seqs. and wait to run rest of script

--- snpGenie_scripts/run_snpGenie_by_pop.sh
+++ snpGenie_scripts/run_snpGenie_by_pop.sh
@@ -22,29 +22,29 @@ module load Python/3.5.2-foss-2016b
 module load Biopython/1.68-foss-2016b-Python-3.5.2
 for ID in $sequence_ids
   do
-    /home/keb27269/projects/EBT/generate_seqs_from_VCF.py ${basedir}/reference_fastas_ORP/noto_1.5.ORP_${ID}.fasta $basedir/variants_Arica/${ID}.Arica.vcf 6
-    /home/keb27269/projects/EBT/generate_seqs_from_VCF.py ${basedir}/reference_fastas_ORP/noto_1.5.ORP_${ID}.fasta $basedir/variants_Argentina/${ID}.Argentina.vcf 7
+    /home/keb27269/projects/EBT/generate_seqs_from_VCF.py ${basedir}/reference_fastas_ORP/noto_1.5.ORP_${ID}.fasta $basedir/variants_Arica/${ID}.Arica.vcf 12
+    /home/keb27269/projects/EBT/generate_seqs_from_VCF.py ${basedir}/reference_fastas_ORP/noto_1.5.ORP_${ID}.fasta $basedir/variants_Argentina/${ID}.Argentina.vcf 14
 done
-
-#move fasta files
-for ID in $sequence_ids
-  do
-    mkdir $basedir/fastas_by_pop/${ID}
-    mv $basedir/variants_Arica/${ID}.Arica_nSeqs6.fasta $basedir/fastas_by_pop/${ID}/
-    mv $basedir/variants_Argentina/${ID}.Argentina_nSeqs7.fasta $basedir/fastas_by_pop/${ID}/
-done
-
-############################################################################
-module load  Parallel-ForkManager/1.19-foss-2016b-Perl-5.24.1
-
-file="cds_seq_ids.txt"
-sequence_ids=$(cat ${file})
-
-#run snpgenie by populations
-#--gtf_file=<CDS_annotations>.gtf --num_bootstraps=10000 --procs_per_node=16
-
-for ID in $sequence_ids
-  do
-    cd $basedir/fastas_by_pop/${ID}/
-    /home/keb27269/projects/SNPGenie/snpgenie_between_group.pl --gtf_file="${basedir}/gff_files/${ID}.new.gtf" --num_bootstraps=10000 --procs_per_node=16
-done
+#
+# #move fasta files
+# for ID in $sequence_ids
+#   do
+#     mkdir $basedir/fastas_by_pop/${ID}_diploid
+#     mv $basedir/variants_Arica/${ID}.Arica_nSeqs12.fasta $basedir/fastas_by_pop/${ID}_diploid/
+#     mv $basedir/variants_Argentina/${ID}.Argentina_nSeqs14.fasta $basedir/fastas_by_pop/${ID}_diploid/
+# done
+#
+# ############################################################################
+# module load  Parallel-ForkManager/1.19-foss-2016b-Perl-5.24.1
+#
+# file="cds_seq_ids.txt"
+# sequence_ids=$(cat ${file})
+#
+# #run snpgenie by populations
+# #--gtf_file=<CDS_annotations>.gtf --num_bootstraps=10000 --procs_per_node=16
+#
+# for ID in $sequence_ids
+#   do
+#     cd $basedir/fastas_by_pop/${ID}/
+#     /home/keb27269/projects/SNPGenie/snpgenie_between_group.pl --gtf_file="${basedir}/gff_files/${ID}.new.gtf" --num_bootstraps=10000 --procs_per_node=16
+# done
