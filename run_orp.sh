22c02e102f55b4a84fe2ba61f116d7f2161a2516
karenbobier <karenbobier@gmail.com>
2020-09-08 15:57:56 -0400

    update script with changes Shan-ho made

--- run_orp.sh
+++ run_orp.sh
@@ -1,17 +1,16 @@
 #PBS -S /bin/bash
-#PBS -q highmem_q
-#PBS -N ORP_noto
-#PBS -l nodes=1:ppn=20
-#PBS -l walltime=30:00:00:00
-#PBS -l mem=400gb
-#PBS -M keb27269@uga.edu
+#PBS -q eupathdb_q
+#PBS -N ORP_test_noto_1
+#PBS -l nodes=1:ppn=28
+#PBS -l walltime=240:00:00
+#PBS -l mem=480gb
+#PBS -M shtsai@uga.edu
 #PBS -m ae
-#PBS -o $HOME/orp_noto.out.$PBS_JOBID
-#PBS -e $HOME/orp_noto.err.$PBS_JOBID
 
-basedir="/scratch/keb27269/noto/"
+#basedir="/scratch/keb27269/noto/"
 #mkdir $basedir
-cd $basedir
+#cd $basedir
+cd $PBS_O_WORKDIR
 
 ml ORP/2.0.0
 
@@ -20,9 +19,11 @@ ml ORP/2.0.0
 source activate orp_v2
 
 $ORPHOME/oyster.mk main \
-MEM=400 \
-CPU=20 \
+MEM=470 \
+CPU=28 \
 READ1=Noto_read_1.fq.gz \
 READ2=Noto_read_2.fq.gz \
-RUNOUT=noto \
+RUNOUT=noto_1.5
+
+#Shan-ho at gacrc helped troubleshoot this script to get ORP to run wihtout errors
 #--dry-run # 1>std.out 2>std.err
