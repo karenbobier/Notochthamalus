9d496810ad6a9203b6f13f85d77d7d8d30672d9a
jpwadmin <jpwadmin@Wares-MacBook-Pro.local>
2020-09-30 15:47:13 -0400

    create scripts to combine outputs of snpgenie analyses

---
+++ snpGenie_scripts/combine_between_group_product_results_files.sh
@@ -0,0 +1,36 @@
+#PBS -S /bin/bash
+#PBS -q highmem_q
+#PBS -N noto_snpgenie
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=20:00:00:00
+#PBS -l mem=100gb
+#PBS -M keb27269@uga.edu
+#PBS -m abe
+#PBS -o $HOME/noto_snpgenie.out.$PBS_JOBID
+#PBS -e $HOME/noto_snpgenie.err.$PBS_JOBID
+
+
+basedir="/scratch/keb27269/noto/snpGenie_test"
+#mkdir $basedir
+cd $basedir
+
+#get population results
+
+#make file header
+cat ${basedir}/fastas_by_pop/J9344521/between_group_product_results.txt  |awk "NR==1" > ${basedir}/noto_between_group_product_results.txt
+#for plus strand seqs
+
+file="seq_ids_plus_strand.txt"
+sequence_ids=$(cat ${file})
+for ID in $sequence_ids
+  do
+    cat ${basedir}/fastas_by_pop/${ID}/between_group_product_results.txt  |awk "NR==2" >> noto_between_group_product_results.txt
+done
+
+#run for reverse complement of minus strand seqs
+#file="seq_ids_minus_strand2.txt"
+#sequence_ids=$(cat ${file})
+#for ID in $sequence_ids
+#do
+#  cat ${basedir}/snp_genie_outputs_ORP/rev_com/${ID}/product_results.txt  |awk "NR==2" >> noto_all_product_results.txt
+#done
