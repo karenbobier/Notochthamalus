daae0a97539ab009108cb2697610b45fc1c2c1b2
jpwadmin <jpwadmin@Wares-MacBook-Pro.local>
2020-09-30 15:46:33 -0400

    create script to blast semibalanus mpi seq against transcriptome

---
+++ blast_isomerase.sh
@@ -0,0 +1,17 @@
+
+
+module load BLAST+/2.9.0-foss-2018a-Python-2.7.14
+module load BLAST+/2.7.1-foss-2016b-Python-2.7.14
+#blastn -num_threads 4 -db <database file> -query <queary file>
+databasefile="/scratch/keb27269/noto/noto_1.5.ORP.fasta.transdecoder.mRNA.fasta"
+queryfile="/scratch/keb27269/noto/isomerase_stuff/semibalanus_MK955540.fasta"
+
+sed 's/_length_.*|//g' < noto_1.5.ORP.fasta.transdecoder.mRNA.fasta >new_mRNA.fasta
+
+database1="/scratch/keb27269/noto/isomerase_stuff/new_mRNA.fasta"
+
+#make blast db
+makeblastdb -in $database1 -input_type fasta -dbtype nucl \
+-title mRNA_for_blastall -parse_seqids -out mRNA_for_blastall
+#run nucleotide blast
+blastn -db mRNA_for_blastall -query $queryfile -evalue 1E-5 -out /scratch/keb27269/noto/isomerase/blast_results.out
